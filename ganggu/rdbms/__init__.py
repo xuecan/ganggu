cca19341835a34ee4d687404ead08af9267ce0cd
Xue Can <xuecan@gmail.com>
2016-09-17 17:08:57 +0800

    make_engine 移到模块 __init__ 中

--- ganggu/rdbms/__init__.py
+++ ganggu/rdbms/__init__.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2014-2016 Xue Can <xuecan@gmail.com> and contributors.
 # Licensed under the MIT license: http://opensource.org/licenses/mit-license
+
+from sqlalchemy import create_engine
+
+
+def make_engine(uri, debug=False):
+    if not uri.startswith("mysql+pymysql://"):
+        raise ValueError("not for MySQL")
+    engine = create_engine(
+        uri,
+        encoding="utf-8",
+        paramstyle="pyformat",
+        isolation_level="READ COMMITTED",
+        echo=debug
+    )
+    return engine
