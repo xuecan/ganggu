8a2e80b80b35766ab03ce9843aca47d07a965eaa
Xue Can <xuecan@gmail.com>
2016-09-20 20:09:03 +0800

    update version number

--- ganggu/__init__.py
+++ ganggu/__init__.py
@@ -2,4 +2,4 @@
 # Copyright (C) 2012-2016 Xue Can <xuecan@gmail.com> and contributors.
 # Licensed under the MIT license: http://opensource.org/licenses/mit-license
 
-__version__ = '0.9.1'
+__version__ = '0.9.2'
