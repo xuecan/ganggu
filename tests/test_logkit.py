f577a9529e0a802102418b64272040640ed58264
Xue Can <xuecan@gmail.com>
2016-12-17 02:38:32 +0800

    年份信息

--- tests/test_logkit.py
+++ tests/test_logkit.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2014-2015 Xue Can <xuecan@gmail.com> and contributors.
+# Copyright (C) 2014-2016 Xue Can <xuecan@gmail.com> and contributors.
 # Licensed under the MIT license: http://opensource.org/licenses/mit-license
 
 import logging
