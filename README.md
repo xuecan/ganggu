193ec43cc742d7d3221239c475388b05aaef9d7e
Xue Can <xuecan@gmail.com>
2016-12-19 02:26:56 +0800

    更新项目说明信息

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ganggu
 
 A random collection of modules work with Flask, SQLAlchemy, Celery and so on.
+The original intention of this is just to save some typing work. Most of modules
+are standalone and can be used independently.
+
+This is for a number of internal projects but also licensed under the MIT license.
--- ganggu/__init__.py
+++ ganggu/__init__.py
@@ -3,10 +3,16 @@
 # Licensed under the MIT license: http://opensource.org/licenses/mit-license
 
 """
-# ganggu
+ganggu
+======
 
-`ganggu` 是一组相对独立的，在日常开发中常常会使用的小模块。每个模块有自身的依赖，如
-Flask、SQLAlchemy 或 Celery 等等，通常用于简化某一方面的打字工作。
+*Ganggu* 是一组相对独立的，在日常开发中常常会使用的小模块。
+
+由于这里的许多代码在一些内部项目中被使用到，最初设立这个项目的意图仅仅是为了\
+节约一些日常的打字工作。许多模块都是独立的，可以单独复制并使用。每个模块的依赖\
+关系在模块文档中有详细说明。
+
+*Ganggu* 的发音 [gɑːŋgu] 在闽南语中意思是工具。
 """
 
 __version__ = '0.10.0'
